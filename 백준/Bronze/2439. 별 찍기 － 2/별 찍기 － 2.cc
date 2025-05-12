0e0cd685c69b8aba2b45bb1fee53eb933ae4b2a5
hasangsu <65026050+hasangsu@users.noreply.github.com>
2025-05-13 08:16:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int count = 0;
+    scanf("%d", &count);
+    
+    for (int line = 1; line <= count; line++)
+    {
+        for (int index = 0; index < (count - line); index++)
+        {
+            printf(" ");
+        }
+        
+        for (int index = 0; index < line; index++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+}
