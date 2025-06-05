fccb443b3dd87178103e162372d3131895f67629
hasangsu <65026050+hasangsu@users.noreply.github.com>
2025-06-06 08:49:18 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::cout << "         ,r'\"7" << "\n";
+    std::cout << "r`-_   ,'  ,/" << "\n";
+    std::cout << " \\. \". L_r'"  << "\n";
+    std::cout << "   `~\\/" << "\n";
+    std::cout << "      |" << "\n";
+    std::cout << "      |" << "\n";
+}
