bdb379dd023187e03e3f670c3c95ccea3cdb746a
hasangsu <65026050+hasangsu@users.noreply.github.com>
2025-11-11 08:35:01 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+
+bool isBetter(int a, int b)
+{
+    int abs_a = std::abs(a);
+    int abs_b = std::abs(b);
+    if (abs_a != abs_b)
+    {
+        return abs_a < abs_b;
+    }
+    
+    return a < b;
+}
+
+void pop(int *heap, int &index)
+{
+    if (index == 0)
+    {
+        std::cout << 0 << "\n";
+        return;
+    }
+    
+    int min = heap[1];
+    heap[1] = heap[index--];
+    
+    int parent = 1;
+    int child = parent * 2;
+    while (child <= index)
+    {
+        if (child + 1 <= index && isBetter(heap[child + 1], heap[child]))
+        {
+            child++;
+        }
+
+        if (!isBetter(heap[child], heap[parent]))
+        {
+            break;
+        }
+        
+        std::swap(heap[parent], heap[child]);
+        parent = child;
+        child = parent * 2;
+    }
+    
+    std::cout << min << "\n";
+}
+
+void push(int *heap, int x, int &index)
+{
+    heap[++index] = x;
+    
+    int child = index;
+    int parent = child / 2;
+    
+    while (child > 1 && isBetter(heap[child], heap[parent]))
+    {
+        std::swap(heap[parent], heap[child]);
+		child = parent;
+		parent = child / 2;
+	}
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    int n = 0;
+    std::cin >> n;
+    
+    int current_index = 0;
+    int heap[n];
+    while (n--)
+    {
+        int x = 0;
+        std::cin >> x;
+        
+        if (x == 0)
+        {
+            // pop
+            pop((int *) heap, current_index);
+        }
+        else
+        {
+            // push
+            push((int *) heap, x, current_index);
+        }
+    }
+}
