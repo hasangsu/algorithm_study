a06edeecd59bb7dc10985be3f3b61624c882a5ea
hasangsu <65026050+hasangsu@users.noreply.github.com>
2026-01-02 12:01:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 116 ms, Memory: 4380 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct edge
+{
+    int s;
+    int e;
+    int w;
+};
+
+bool compare(const edge &a, const edge &b)
+{
+    return a.w < b.w;
+}
+
+int findParent(int x, std::vector<int> &parent) 
+{
+    if (parent[x] == x) 
+    {
+        return x;
+    }
+    
+    return parent[x] = findParent(parent[x], parent);
+}
+
+void unionParent(int a, int b, std::vector<int> &parent)
+{
+    a = findParent(a, parent);
+    b = findParent(b, parent);
+    if (a < b)
+    {
+        parent[b] = a;
+    }
+    else
+    {
+        parent[a] = b;
+    }
+}
+
+int main()
+{
+    int V = 0;
+    int E = 0;
+    std::cin >> V >> E;
+    
+    std::vector<edge> edges;
+    std::vector<int> parent(V + 1);
+    for (int i = 0; i <= V; i++)
+    {
+        parent[i] = i;
+    }
+    
+    for (int i = 0; i < E; i++)
+    {
+        int s = 0;
+        int e = 0;
+        int w = 0;
+        std::cin >> s >> e >> w;
+        
+        edges.push_back({s, e, w});
+    }
+    
+    std::sort(edges.begin(), edges.end(), compare);
+    
+    int result = 0;
+    int count = 0;
+    for (const edge &target : edges)
+    {
+        int s = target.s;
+        int e = target.e;
+        int w = target.w;
+        
+        int p1 = findParent(s, parent);
+        int p2 = findParent(e, parent);
+        if (p1 != p2)
+        {
+            unionParent(p1, p2, parent);
+            result += w;
+            count++;
+        }
+        
+        if (count == V - 1)
+        {
+            break;
+        }
+    }
+    
+    std::cout << result;
+}
